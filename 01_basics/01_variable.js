9134d84a7289d5d6e366ca4f0604ecd118f123cd
nickyy25 <142549368+nickyy25@users.noreply.github.com>
2024-07-30 04:39:32 +0000

    content config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "nicky@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+accountEmail = "nk@nk.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountCity]);
+
