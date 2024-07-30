416b13eb440d4bba6e3146a49bf9d7b53afeea5e
nickyy25 <142549368+nickyy25@users.noreply.github.com>
2024-07-30 05:15:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS CODE as new version
+
+// alert (3+3) // we are using node js not browser
+// code readability should be high
+// mdn for js documentation
+
+let name = "Nicky" // string
+let age = 18 // num
+let isLoggedIn = true // boolean
+
+//  Primitive Data types
+//number => range 2^53 
+// bigint => for large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value (empty)
+// undefined => value not assinged
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof null); // o/p object
+console.log(typeof undefined); // o/p undefined
+
+
+
