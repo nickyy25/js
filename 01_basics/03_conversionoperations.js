6c75c06f9c1e30248321815e4f8e92800e9e5db6
nickyy25 <142549368+nickyy25@users.noreply.github.com>
2024-08-02 22:49:12 +0000

    operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,14 +1,64 @@
 // let score = "33abc"
 let score = null
 
-console.log(typeof score);
- console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number (score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
 
 // "33" => 33
 // "33abc => NaN
 // True => 1; false => 0
+
+let isLoggedIn = "Nicky"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0=> false
+// "" => false
+// "nicky" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *****************OPERATIONS*************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Nicky"
+// add two strings
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+//console.log("1" + "2");
+// console.log("1" + 2 + 2); 
+
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter = 100
+//gameCounter++;    // postfix
+++gameCounter;     //  prefix
+console.log(gameCounter);
+
 
